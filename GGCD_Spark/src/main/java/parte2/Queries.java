ca3e328d7845f85de93a4801ecac9b17b3e6be14
brunocv <brunocveloso7@gmail.com>
2021-05-31 17:23:35 +0100

    parte 2 feita

--- GGCD_Spark/src/main/java/parte2/Queries.java
+++ GGCD_Spark/src/main/java/parte2/Queries.java
@@ -3,6 +3,7 @@ package parte2;
 import com.google.common.collect.Iterables;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
@@ -47,24 +48,25 @@ public class Queries {
         List<Tuple2<Object, Object>> query1_result = query1.collect();
         query1_result.forEach(System.out::println);
 
+
+
         // ############################################### QUERY 2 ############################################
 
-        JavaPairRDD<String, Integer> query2_basics = spark.table("title_basics_parquet").toJavaRDD()
-                .filter(l -> !l.isNullAt(5))
-                .mapToPair(l -> new Tuple2<>(l.getString(0), l.getInt(5)));
+        JavaPairRDD<String, Tuple2<Integer,String>> query2_basics = spark.table("title_basics_parquet").toJavaRDD()
+                .filter(l -> !l.isNullAt(2) && !l.isNullAt(5))
+                .mapToPair(l -> new Tuple2<>(l.getString(0), new Tuple2<>(l.getInt(5),l.getString(2))));
 
-        JavaPairRDD<String, BigDecimal> query2_ratings = spark.table("title_ratings_parquet").toJavaRDD()
+        JavaPairRDD<String, BigDecimal> query2_ratings = spark.table("title_ratings_parquet_fixed").toJavaRDD()
                 .filter(l -> !l.isNullAt(1))
                 .mapToPair(l -> new Tuple2<>(l.getString(0), l.getDecimal(1)));
 
-        JavaPairRDD<Integer, Object> query2_best = query2_basics.join(query2_ratings)
-                .mapToPair(l -> new Tuple2<>(l._2()._2(), new Tuple2<>(l._2()._1(), l._1())))
-                .sortByKey(false)
-                .mapToPair(l -> new Tuple2<>(l._2()._1(), new Tuple2<>(l._1(), l._2()._2())))
-                .groupByKey().sortByKey(false)
-                .mapToPair(l -> new Tuple2<>(l._1(),Iterables.get(l._2(),0)));
+        JavaPairRDD<Integer, Tuple2<String,BigDecimal>> query2_results = query2_basics.join(query2_ratings) //id,((ano, titulo), rating)
+                .mapToPair(l -> new Tuple2<>(l._2._1._1, new Tuple2<>(l._2._1._2, l._2._2))) //ano (titulo,rating)
+                .reduceByKey((Function2<Tuple2<String, BigDecimal>, Tuple2<String, BigDecimal>, Tuple2<String, BigDecimal>>)
+                       (i1, i2) -> i1._2.floatValue() > i2._2.floatValue() ? i1 : i2)
+                .sortByKey(false);
 
-        List<Tuple2<Integer, Object>> query2_result = query2_best.collect();
+        List<Tuple2<Integer, Tuple2<String, BigDecimal>>> query2_result = query2_results.collect();
 
         query2_result.forEach(System.out::println);
 
@@ -79,6 +81,8 @@ public class Queries {
                 .take(10);
 
         query3.forEach(System.out::println);
+
+
 
 
 
